b99641ff7f4874c8d16dfac50462471997a11cbc
AbdelrahmanAbdelfattah <abdelrahmanmohamed702413@gmail.com>
2023-07-17 00:56:46 +0300

    CORS ORIGIN

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,21 @@
 // this is the express file
 
 const express = require("express");
+const cors = require("cors");
 const planetsRouter = require("./routes/planets/planets.router");
-const planetRouter = require("./routes/planets/planets.router.js");
 
 const app = express();
 
 module.exports = app;
 
+// Cross-Origin (or Cross-Origin Resource Sharing - CORS) is a security feature implemented by web browsers
+// to restrict web pages from making requests to a different domain than the one that served the web page.
+// It is a fundamental security concept of the Same-Origin Policy (SOP), which helps prevent malicious websites
+// from accessing sensitive data on other websites.
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 app.use(express.json());
-
 app.use(planetsRouter);
