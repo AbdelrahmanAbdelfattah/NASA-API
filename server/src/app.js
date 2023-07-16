c894081529874ea020c0b493fbbf9c0c029975f5
AbdelrahmanAbdelfattah <abdelrahmanmohamed702413@gmail.com>
2023-07-16 23:44:04 +0300

    GET planets

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,13 @@
 // this is the express file
 
 const express = require("express");
+const planetsRouter = require("./routes/planets/planets.router");
+const planetRouter = require("./routes/planets/planets.router.js");
 
 const app = express();
 
 module.exports = app;
+
+app.use(express.json());
+
+app.use(planetsRouter);
