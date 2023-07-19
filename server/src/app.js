13e43d19344b79cfc41edfc6bffd9ab39faefd45
AbdelrahmanAbdelfattah <abdelrahmanmohamed702413@gmail.com>
2023-07-19 22:18:57 +0300

    Serving React.js Front End In Production

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const cors = require("cors");
 const planetsRouter = require("./routes/planets/planets.router");
+const path = require("path");
 
 const app = express();
 
@@ -18,4 +19,9 @@ app.use(
   })
 );
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
+
 app.use(planetsRouter);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
