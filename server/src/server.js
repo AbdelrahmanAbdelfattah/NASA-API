96e0d4e6664e784ba0bacc84b85234ad95f86656
AbdelrahmanAbdelfattah <abdelrahmanmohamed702413@gmail.com>
2023-08-11 02:37:25 +0300

    Mongodb connection

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,7 @@
 const http = require("http");
 
+const mongoose = require("mongoose");
+
 const app = require("./app");
 
 const { loadPlanetsData } = require("./models/planets.model");
@@ -11,7 +13,28 @@ const PORT = process.env.PORT || 8000;
 //  for incoming requests and forward them to the appropriate route handlers and middleware defined in your Express app.
 const server = http.createServer(app);
 
+//once is a function of event emmitter that run one time when the the event (open) is happened
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready!");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
+const MONGO_URL =
+  "mongodb+srv://nasa-api:DbL41bUbOkLo1Klr@nasacluster.ul9mdcg.mongodb.net/NASACluster?retryWrites=true&w=majority";
+
 async function startServer() {
+  // it returns a promise so we use await here
+  await mongoose.connect(MONGO_URL, {
+    //this determines how Mongoose parsses that connection string into URL (MONGO_URL)
+    useNewUrlParser: true,
+    // useFindAndModify: false,
+    // useCreateIndex: true,
+    useUnifiedTopology: true,
+  });
+
   await loadPlanetsData();
 
   server.listen(PORT, () => {
