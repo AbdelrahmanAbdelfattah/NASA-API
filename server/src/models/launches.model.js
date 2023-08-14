8df8588034d77f2ffd2ed70f826cbb9ec4c5688e
AbdelrahmanAbdelfattah <abdelrahmanmohamed702413@gmail.com>
2023-08-14 22:20:24 +0300

    Sorting Paginated Data

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -4,19 +4,6 @@ const axios = require("axios");
 
 const DEFAULT_FLIGHT_NUMBER = 100;
 
-const launch = {
-  flightNumber: 100, //flight_number
-  mission: "Kepler Exploration X", //name
-  rocket: "Explorer IS1", //rocket.name
-  lunchDate: new Date("December 27, 2030"), //date_local
-  target: "Kepler-442 b", // not applicable
-  customers: ["ZTM", "NASA"], //payload.customers for each payload
-  upcoming: true, //upcoming
-  success: true, //success
-};
-
-saveLaunch(launch);
-
 const SPACEX_API_URL = "https://api.spacexdata.com/v4/launches/query";
 
 async function populateLaunches() {
@@ -104,6 +91,7 @@ async function getLatestFlightNumber() {
 async function getAllLaunches(skip, limit) {
   return await launchesDatabase
     .find({}, { _id: 0, __v: 0 })
+    .sort({ flightNumber: 1 })
     .skip(skip)
     .limit(limit);
 }
